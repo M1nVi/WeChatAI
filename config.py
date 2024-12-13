15f380c131de72a6e5434a966e61da442aa6798d
M1nVi <1789019113@qq.com>
2024-12-13 10:07:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ DEBUG = True
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'root')
 db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
-api_key = os.environ.get("ALIYUN_API_KEY",'sk-16bc5eca975e4e1baabae888594d4e01')
+AI_API_KEY = os.getenv("AI_API_KEY", "sk-16bc5eca975e4e1baabae888594d4e01")
+AI_API_URL = os.getenv("AI_API_URL", "https://api.openai.com/v1/completions")
