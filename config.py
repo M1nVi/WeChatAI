f78e8d4595fc03d1f7802f994087e8cb10a535e1
M1nVi <1789019113@qq.com>
2024-12-16 16:53:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     username = os.environ.get("MYSQL_USERNAME", 'root')
     password = os.environ.get("MYSQL_PASSWORD", 'root')
     db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
-    AI_API_KEY = os.getenv("AI_API_KEY", "sk-16bc5eca975e4e1baabae888594d4e01")
-    AI_API_URL = os.getenv("AI_API_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions")
+    ai_key = os.getenv("AI_API_KEY")
+    ai_url = os.getenv("AI_API_URL")
