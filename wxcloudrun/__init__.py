f5207080809d3b14c6a5f588281bb71524ef647d
M1nVi <1789019113@qq.com>
2024-12-16 15:18:18 +0800

    Update __init__.py

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -1,24 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
-from config import Config
+from config import Config  # 引入封装的 Config 类
 
 # 因MySQLDB不支持Python3，使用pymysql扩展库代替MySQLDB库
 pymysql.install_as_MySQLdb()
 
-# 初始化web应用
-app = Flask(__name__, instance_relative_config=True)
-app.config['DEBUG'] = config.DEBUG
+# 初始化 web 应用
+app = Flask(__name__)
 
-# 设定数据库链接
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/flask_demo'.format(config.username, config.password,
-                                                                             config.db_address)
+# 使用 Config 类配置 Flask 应用
+app.config.from_object(Config)
 
-# 初始化DB操作对象
+# 初始化数据库配置
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{Config.username}:{Config.password}@{Config.db_address}/flask_demo"
+
+# 初始化 DB 操作对象
 db = SQLAlchemy(app)
 
 # 加载控制器
 from wxcloudrun import views
-
-# 加载配置
-app.config.from_object('Config')
