ca1ffd4993dc3d900ae1caf6f9b76e0f6c0e109c
M1nVi <1789019113@qq.com>
2024-12-16 15:16:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 
-# 是否开启debug模式
-DEBUG = True
-
 class Config:
     # 读取数据库环境变量
+    DEBUG = os.getenv('FLASK_DEBUG', True)
     username = os.environ.get("MYSQL_USERNAME", 'root')
     password = os.environ.get("MYSQL_PASSWORD", 'root')
     db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
